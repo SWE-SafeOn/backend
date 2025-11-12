7a206be85216b3a39ba523f341204cecb45511ea
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-12 14:50:39 +0900

    refactor: dto의 id string으로 타입 수정

--- src/main/java/com/example/demo/dto/user/LoginRequestDto.java
+++ src/main/java/com/example/demo/dto/user/LoginRequestDto.java
@@ -4,4 +4,5 @@ public record LoginRequestDto(
     String email, 
     String password
 ) {
+    
 }
--- src/main/java/com/example/demo/dto/user/UserResponseDto.java
+++ src/main/java/com/example/demo/dto/user/UserResponseDto.java
@@ -3,18 +3,17 @@ package com.example.demo.dto.user;
 import com.example.demo.entity.User;
 import lombok.Builder;
 
-import java.util.UUID;
 
 @Builder
 public record UserResponseDto(
-        UUID id,
+        String id,
         String email,
         String name,
         String created_at
 ) {
     public static UserResponseDto from(User user) {
         return UserResponseDto.builder()
-                .id(user.getId())
+                .id(user.getId().toString())
                 .email(user.getEmail())
                 .name(user.getName())
                 .created_at(user.getCreatedAt().toString())

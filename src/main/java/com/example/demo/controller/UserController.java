08031b1e44265dda8c77534412732ed1fe0a75ac
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-12 12:04:23 +0900

    docs: swagger tag name 수정

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,14 +4,18 @@ import com.example.demo.dto.user.LoginRequestDto;
 import com.example.demo.dto.user.SignUpRequestDto;
 import com.example.demo.dto.user.UserResponse;
 import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/auth")
+@Tag(name = "User Auth")
 public class UserController {
 
     private final UserService userService;
